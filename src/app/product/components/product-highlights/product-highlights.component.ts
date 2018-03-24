5e057592d854229b40c98c7bf2d3b5a765799759
Unknown <dfederico7@gmail.com>
2018-03-24 17:43:09 -0500

    Adding product highlights

--- src/app/product/components/product-highlights/product-highlights.component.ts
+++ src/app/product/components/product-highlights/product-highlights.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ItemDescriptionEntity } from '../../models/Product';
 
 @Component({
   selector: 'mr-product-highlights',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductHighlightsComponent implements OnInit {
 
+  @Input('highlights') highlights: Array<ItemDescriptionEntity>;
+
   constructor() { }
 
   ngOnInit() {
+    console.log(this.highlights);
   }
 
 }
